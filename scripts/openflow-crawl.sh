d64e715d1c40c40a9412d6094fd005ae43362581
Dragos Dumitrescu <dragostennis1992@yahoo.com>
2017-05-04 13:05:16 +0300

    Stack script - rework

--- scripts/openflow-crawl.sh
+++ scripts/openflow-crawl.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 ns="root"
-ip netns > namespaces-`hostname`.txt
-ip address show > ifaces-$ns-`hostname`.txt
-route -n > routes-$ns-`hostname`.txt
-iptables -t raw -S > iptables-raw-$ns-`hostname`.txt
-iptables -t nat -S > iptables-nat-$ns-`hostname`.txt
-iptables -t filter -S > iptables-filter-$ns-`hostname`.txt
-iptables -t mangle -S > iptables-mangle-$ns-`hostname`.txt
-brctl show > linuxbridges-$ns-`hostname`.txt
+ip netns > symnet-namespaces-`hostname`.txt
+ip address show > symnet-ifaces-$ns-`hostname`.txt
+route -n > symnet-routes-$ns-`hostname`.txt
+iptables -t raw -S > symnet-iptables-raw-$ns-`hostname`.txt
+iptables -t nat -S > symnet-iptables-nat-$ns-`hostname`.txt
+iptables -t filter -S > symnet-iptables-filter-$ns-`hostname`.txt
+iptables -t mangle -S > symnet-iptables-mangle-$ns-`hostname`.txt
+brctl show > symnet-linuxbridges-$ns-`hostname`.txt
 for ns in `ip netns`;
 do
-    ip netns exec $ns ip address show > ifaces-$ns-`hostname`.txt
-    ip netns exec $ns route -n > routes-$ns-`hostname`.txt
-    ip netns exec $ns iptables -t raw -S > iptables-raw-$ns-`hostname`.txt
-    ip netns exec $ns iptables -t nat -S > iptables-nat-$ns-`hostname`.txt
-    ip netns exec $ns iptables -t filter -S > iptables-filter-$ns-`hostname`.txt
-    ip netns exec $ns iptables -t mangle -S > iptables-mangle-$ns-`hostname`.txt
-    ip netns exec $ns brctl show > linuxbridges-$ns-`hostname`.txt
+    ip netns exec $ns ip address show > symnet-ifaces-$ns-`hostname`.txt
+    ip netns exec $ns route -n > symnet-routes-$ns-`hostname`.txt
+    ip netns exec $ns iptables -t raw -S > symnet-iptables-raw-$ns-`hostname`.txt
+    ip netns exec $ns iptables -t nat -S > symnet-iptables-nat-$ns-`hostname`.txt
+    ip netns exec $ns iptables -t filter -S > symnet-iptables-filter-$ns-`hostname`.txt
+    ip netns exec $ns iptables -t mangle -S > symnet-iptables-mangle-$ns-`hostname`.txt
+    ip netns exec $ns brctl show > symnet-linuxbridges-$ns-`hostname`.txt
 done
+
+
+ovsdb-client dump -f json --pretty > symnet-ovs-`hostname`.txt
 
 for x in `ovs-vsctl list-br`; 
 do
-ovsdb-client dump -f json --pretty > ovs-`hostname`.txt
-ovs-ofctl dump-ports-desc $x > ports-$x-`hostname`.txt
-ovs-ofctl dump-flows $x > flows-$x-`hostname`.txt
+ovs-ofctl dump-ports-desc $x > symnet-ports-$x-`hostname`.txt
+ovs-ofctl dump-flows $x > symnet-flows-$x-`hostname`.txt
 done
