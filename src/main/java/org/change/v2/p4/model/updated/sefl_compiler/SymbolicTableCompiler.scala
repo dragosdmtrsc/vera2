e8d53a547da876d67bfab32db2a265aecf82cd4f
Radu Stoenescu <radu.stoe@gmail.com>
2017-12-08 10:44:01 +0200

    +

--- src/main/java/org/change/v2/p4/model/updated/sefl_compiler/SymbolicTableCompiler.scala
+++ src/main/java/org/change/v2/p4/model/updated/sefl_compiler/SymbolicTableCompiler.scala
@@ -1,13 +1,19 @@
 package org.change.v2.p4.model.updated.sefl_compiler
 
+import org.change.v2.analysis.expression.abst.Expression
+import org.change.v2.analysis.expression.concrete.SymbolicValue
+import org.change.v2.p4.model.actions.{P4Action, P4ActionParameter}
 import org.change.v2.p4.model.updated.program.P4Program
-import org.change.v2.p4.model.updated.sefl_compiler.sefl_code.{EmptySEFLProgram, SEFLCode}
+import org.change.v2.p4.model.updated.sefl_compiler.sefl_code.{EmptySEFLProgram, SEFLCode, SEFLCodeContainer}
 import org.change.v2.p4.model.updated.table.{BasicActionSpecification, TableDeclaration}
 
 object SymbolicTableCompiler extends SEFLCompiler[P4Program] {
 
   override def compile(p4: P4Program): SEFLCode = {
-    for {
+    /**
+      * For every table, symbolic match values and action parameters are allocated.
+      */
+    val matchValues: Map[String, Expression] = (for {
       source <- p4.tableDeclarations.values
       tableName = source.tableName
     } yield source.allowedActions match {
@@ -15,9 +21,34 @@ object SymbolicTableCompiler extends SEFLCompiler[P4Program] {
         action <- basicActionSpec.actionRefs
         matchCondition <- source.tableMatches
         symbolName = s"$tableName-${action.name}-${matchCondition.what.name}"
-      } println(symbolName)
-    }
+      } yield symbolName -> SymbolicValue()
+      // TODO: Non basic action specification
+      case _ => ???
+    }).flatten.toMap
 
-    EmptySEFLProgram
+    println("===")
+
+    import collection.JavaConverters._
+    val symbolicParameters = (for {
+      source <- p4.tableDeclarations.values
+      tableName = source.tableName
+    } yield source.allowedActions match {
+      case basicActionSpec: BasicActionSpecification => for {
+        actionRef <- basicActionSpec.actionRefs
+        actionId = actionRef.name
+        action: P4Action = p4.complexActions.getOrElse(actionId, p4.primitiveActions(actionId))
+        actionParameter: P4ActionParameter <- action.getParameterList.asScala
+        symbolName = s"$tableName-$actionId-${actionParameter.getParamName}"
+      } yield symbolName -> SymbolicValue()
+      // TODO: Non basic action specification
+      case _ => ???
+    }).flatten.toMap
+
+    assert((matchValues.keys.toSet intersect symbolicParameters.keys.toSet).isEmpty)
+
+    SEFLCodeContainer(
+      instructions = Map.empty,
+      symbolTable = (matchValues ++ symbolicParameters).mapValues(e => (e, None)) // these symbols do not get offsets
+    )
   }
 }
