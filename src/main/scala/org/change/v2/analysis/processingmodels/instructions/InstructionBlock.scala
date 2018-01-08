9ab3b74d9e747997167a774007f99626b8f6b5ac
Radu Stoenescu <radu.stoe@gmail.com>
2018-01-08 11:01:26 +0200

    +

--- src/main/scala/org/change/v2/analysis/processingmodels/instructions/InstructionBlock.scala
+++ src/main/scala/org/change/v2/analysis/processingmodels/instructions/InstructionBlock.scala
@@ -25,7 +25,7 @@ case class InstructionBlock(instructions: Iterable[Instruction]) extends Instruc
     }
 
   override def toString: String = {
-    "{\n" + instructions.map(_.toString).mkString("\n") + "\n}"
+    "{\n" + instructions.mkString("\n") + "\n}"
   }
 }
 
