5cba40aed8c7e5bccc3a6135e1ceaf7f652983cf
Unknown <dragostennis1992@yahoo.com>
2019-02-11 12:38:33 +0200

    + sefl dumper

--- src/main/scala/org/change/v2/tools/sefl/ToSEFL.scala
+++ src/main/scala/org/change/v2/tools/sefl/ToSEFL.scala
@@ -2,30 +2,28 @@ package org.change.v2.tools.sefl
 
 import java.io.{BufferedOutputStream, FileOutputStream, PrintWriter}
 
-import org.change.v2.cmd.EQParams
 import org.change.v2.plugins.eq.{PluginHolder, TopologyPlugin}
-import org.change.v2.tools.eq.Equivalence.{findField, isScalar}
 import org.change.v2.util.ToDot
 
-case class DotArgs(topoClass : String = "",
+case class ToSEFLArgs(topoClass : String = "",
                    topoParms : Map[String, String] = Map.empty,
-                   startFrom : String = ".*",
-                   outDot : String = "")
+                   startFrom : String = ".*", outFile : String = "")
 
-object Sefl2Dot {
+object ToSEFL {
   val usage =
     """--topology-plugin <plugin_class>
 [--topology-parm <parm_name> <parm_value>]*
 [--start-from <regex>]?
---out-dot <file_name>
+--out-file <file_name>
     """.stripMargin
 
   def main(args: Array[String]): Unit = {
+    val arglist = args.toList
     if (args.length < 4) {
       System.err.println(usage)
       System.exit(1)
     }
-    def nextOption(eqParams : DotArgs, list: List[String]) : DotArgs = {
+    def nextOption(eqParams : ToSEFLArgs, list: List[String]) : ToSEFLArgs = {
       list match {
         case Nil => eqParams
         case "--topology-plugin" :: value :: tail => nextOption(eqParams.copy(topoClass = value), tail)
@@ -33,19 +31,20 @@ object Sefl2Dot {
           nextOption(eqParams.copy(topoParms = eqParams.topoParms + (name -> value)), tail)
         case "--start-from" :: regex :: tail =>
           nextOption(eqParams.copy(startFrom = regex), tail)
-        case "--out-dot" :: file :: tail =>
-          nextOption(eqParams.copy(outDot = file), tail)
+        case "--out-file" :: file :: tail =>
+          nextOption(eqParams.copy(outFile = file), tail)
       }
     }
-    val options = nextOption(DotArgs(), args.toList)
-    if (options.outDot.isEmpty || options.topoClass.isEmpty) {
+    val options = nextOption(ToSEFLArgs(), args.toList)
+    if (options.outFile.isEmpty || options.topoClass.isEmpty) {
       System.err.println(usage)
       System.exit(1)
     }
     val topo = PluginHolder.instantiate[TopologyPlugin](options.topoClass, options.topoParms)
     val g = topo()
-    val os = new PrintWriter(new BufferedOutputStream(new FileOutputStream(options.outDot)))
+    val os = new PrintWriter(new BufferedOutputStream(new FileOutputStream(options.outFile)))
     ToDot.apply("topology", g.toMap, topo.startNodes().filter(_.matches(options.startFrom)).toSet, os)
     os.close()
   }
+
 }
---
+++ src/main/scala/org/change/v2/util/ToSEFL.scala
@@ -0,0 +1,66 @@
+package org.change.v2.util
+
+import java.io.PrintWriter
+
+import org.change.v2.analysis.expression.abst.FloatingExpression
+import org.change.v2.analysis.expression.concrete.nonprimitive.:@
+import org.change.v2.analysis.memory.Intable
+import org.change.v2.analysis.processingmodels.Instruction
+import org.change.v2.analysis.processingmodels.instructions._
+import org.change.v2.util.ToDot.{mkIPCG, normalize}
+
+import scala.collection.mutable
+
+object ToSEFL {
+  def handleBlock(port : String,
+                  stack : mutable.Buffer[String],
+                  visited : mutable.Set[String],
+                  instructions : Map[String, Instruction],
+                  outputStream : PrintWriter) : Unit = {
+
+    def handle(constrain : ConstrainFloatingExpression) : String = {
+      ???
+    }
+    def handleCondition(instruction: Instruction) : String = {
+      ???
+    }
+    def handleAssign(n : Either[String, Intable], f : FloatingExpression) : String = {
+      ???
+    }
+
+    def visit(instruction: Instruction) : String = instruction match {
+      case InstructionBlock(is) => "{\n" + is.map(visit).mkString("\n") + "}"
+      case Fork(is) => is.map(visit).mkString(" ||\n")
+      case AssignNamedSymbol(n, x, _) => handleAssign(Left(n), x)
+      case AssignRaw(n, x, _) => handleAssign(Right(n), x)
+      case If(a, b, NoOp) => "if (" + handleCondition(a) + ")\n" + visit(b)
+      case If(a, b, c) => "if (" + handleCondition(a) + ")\n" + visit(b) + "\nelse\n" + visit(c)
+      case NoOp => "nop"
+      case c : ConstrainFloatingExpression => handle(c)
+      case ConstrainNamedSymbol(x, c, _) => visit(ConstrainFloatingExpression(:@(x), c))
+      case ConstrainRaw(x, c, _) => visit(ConstrainFloatingExpression(:@(x), c))
+      case Assume(x) => "assume(" + handleCondition(x) + ")"
+      case Forward(loc) =>
+        if (!visited.contains(loc))
+          stack.append(loc)
+        s"forward $loc"
+    }
+    visited.add(port)
+    if (instructions.contains(port))
+      outputStream.println("block " + port + " {\n" + visit(instructions(port)) + "\n" + "}")
+    else
+      outputStream.println("")
+  }
+
+  def apply(instructions : Map[String, Instruction],
+            startingFrom : Set[String] = Set.empty,
+            outputStream : PrintWriter): Unit = {
+    val stack = mutable.ListBuffer.empty[String]
+    val visited = mutable.Set.empty[String]
+    stack.appendAll(startingFrom)
+    while (stack.nonEmpty) {
+      val top = stack.remove(0)
+      handleBlock(top, stack, visited, instructions, outputStream)
+    }
+  }
+}
